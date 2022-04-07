5ab8a76714992c319e8401fd0824e122cf0425db
Abel <abelamapip@gmail.com>
2022-04-07 16:09:21 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * printf - prints whats given
+ * Return: exit code
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
