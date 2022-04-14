3c3079bfc5ab2a836092c2568a6f1588cd4371fc
Abel <abelamapip@gmail.com>
2022-04-14 03:51:01 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+/**
+ * print_alphabet - prints lowercase alphabets
+ */
+void print_alphabet(void)
+{
+int a = 97;
+while (a < 123)
+putchar(a++);
+putchar(10);
+}
