cb58fc9b3250d1de47150144e387394a4c38ed28
Abel <abelamapip@gmail.com>
2022-04-17 21:48:28 +0300

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_islower - checks for llowercase
+ *Return: 1 for lowercase 0 for otherwise
+ */
+int _islower(int c)
+{
+if (c > 96 && c < 123)
+return (1);
+else
+return (0);
+}
