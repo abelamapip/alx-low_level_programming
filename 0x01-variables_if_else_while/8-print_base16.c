7e364290c1a9473f634cab7475229e913685b6b8
Abel <abelamapip@gmail.com>
2022-04-17 15:28:55 +0300

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - entry function
+ *Return: 0
+ */
+int main(void)
+{
+int a;
+for (a = 48; a < 58 ; a++)
+putchar(a);
+for (a = 97; a < 103 ; a++)
+putchar(a);
+putchar(10);
+return (0);
+}
