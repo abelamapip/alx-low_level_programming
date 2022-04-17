6511e27f4d2c54cd62d2f1e56cc84978ff9478b5
Abel <abelamapip@gmail.com>
2022-04-17 15:11:31 +0300

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - entry function
+ *Return: 0
+ */
+int main(void)
+{
+int a;
+for (a = 97; a < 123 ; a++)
+{
+if (a != 101 && a != 113)
+putchar(a);
+}putchar(10);
+return (0);
+}
