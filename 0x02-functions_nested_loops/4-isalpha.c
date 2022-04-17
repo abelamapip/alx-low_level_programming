f7cd3e8acf297f415365aa52576cc3f9ed6d0e7c
Abel <abelamapip@gmail.com>
2022-04-17 23:03:54 +0300

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *_islower - checks for llowercase
- *Return: 1 for lowercase 0 for otherwise
+ *_isalpha - checks for latter
+ *Return: 1 for latter 0 for otherwise
  *@c: the character to print
  */
 int _isalpha(int c)
 {
-if (c > 96 && c < 123 || c > 64 && c < 91)
+if ((c > 96 && c < 123) || (c > 64 && c < 91))
 return (1);
 else
 return (0);
