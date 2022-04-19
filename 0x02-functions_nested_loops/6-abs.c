95457f26f5f614065bffc8917fc3319e583fe1e4
Abel <abelamapip@gmail.com>
2022-04-19 14:00:10 +0300

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_abs - return the absolute value of a number
+ *@a: input number
+ *Return: returns the absolute value 
+ */
+int _abs(int a)
+{
+if(a<0)
+a = a * -1;
+return (a);
+}
