10a09b7f3ae5433896212b527f698b00b8465b76
Abel <abelamapip@gmail.com>
2022-04-19 14:03:12 +0300

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,11 @@
 /**
  *_abs - return the absolute value of a number
  *@a: input number
- *Return: returns the absolute value 
+ *Return: returns the absolute value
  */
 int _abs(int a)
 {
-if(a<0)
+if (a < 0)
 a = a * -1;
 return (a);
 }
