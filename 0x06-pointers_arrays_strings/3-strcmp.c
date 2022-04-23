16ed9abaf6bd6ee40960246a2897a274e635979e
Abel <abelamapip@gmail.com>
2022-04-23 06:45:27 +0300

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,14 +5,15 @@
  *@s2: the second string
  *Return: the concatenaded string
  */
-int *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
-int i;
+int i, a = 0;
 for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 {
 if (s1[i] != s2[i])
 {
-return (s1[i] - s2[i]);
+a = s1[i] - s2[i];
+return (a);
 }
 }
 return (0);
