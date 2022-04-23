79d20dfb352170d0afa476b0f5199a8898c6fb27
Abel <abelamapip@gmail.com>
2022-04-23 05:55:29 +0300

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,24 @@
 #include "main.h"
-char *_strncat(char *dest, char *str,int n)
+/**
+ *_strcat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: int value
+ *Return: the concatenaded string
+ */
+char *_strncat(char *dest, char *src, int n)
 {
-
+int a = 0, b;
+while (dest[a] != '\0')
+a++;
+for (b = 0; b < n; b++)
+{
+if (src[b] == '\0')
+{
+dest[a] = src[b];
+break;
+}
+dest[a++] = src[b];
+}
+return (dest);
 }
